3ddc99d24600b2dd03cf87df82f31bde91424455
Felipe Melo <felipemelogomes36@gmail.com>
2023-12-11 22:29:21 -0300

    feat: add yup validations

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { CidadesController } from "./../controllers";
+
 const router = Router();
 
 router.get("/", (_, res) => {
   return res.send("Olá DEV!");
 });
 
-router.post("/teste", (req, res) => {
-  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-});
+router.post("/cidades", CidadesController.create);
 
 export { router };
