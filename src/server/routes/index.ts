b59f7b523e997578c5edf2fc0c40b9acb4ad4116
Felipe Melo <felipemelogomes36@gmail.com>
2023-12-12 17:22:05 -0300

    feat: added getById, deleteById and updateById to cidades controller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -13,6 +13,21 @@ router.get(
   CidadesController.getAllValidation,
   CidadesController.getAll
 );
+router.get(
+  "/cidades/:id",
+  CidadesController.getByIdValidation,
+  CidadesController.getById
+);
+router.put(
+  "/cidades/:id",
+  CidadesController.updateByIdValidation,
+  CidadesController.updateById
+);
+router.delete(
+  "/cidades/:id",
+  CidadesController.deleteByIdValidation,
+  CidadesController.deleteById
+);
 router.post(
   "/cidades",
   CidadesController.createValidation,
